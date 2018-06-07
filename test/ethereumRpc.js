9e5541fe46d446d30ab478826c84f69b4075fd92
Jason Smythe <jason@smythemail.za.net>
2018-06-07 23:21:48 +0200

    [closes #2] Fix the tests - run in remote docker mode instead

--- test/ethereumRpc.js
+++ test/ethereumRpc.js
@@ -1,5 +1,6 @@
 const request = require('request');
 
+// helper function that returns a promise of the result of an ethereum-rpc call to a specified url
 function rpcRequest(url, body) {
   const options = {
     url,
@@ -11,11 +12,11 @@ function rpcRequest(url, body) {
     body
   }
   return new Promise(function(resolve, reject){
-    request(options, (error, response, body) => {
+    request(options, (error, response, responseBody) => {
         if (error) {
           reject('Connect Error: ' + error.toString())
         } else {
-          resolve(body)
+          resolve(responseBody)
         }
     })
   })
--- test/testEthereumNetwork.js
+++ test/testEthereumNetwork.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect
-const ethereumRpcTester = require('./ethereumRpcTester')
+const ethereumRpc = require('./ethereumRpc')
 
 const node1RpcUrl = 'http://172.14.0.2:20010'
 const node2RpcUrl = 'http://172.14.0.3:20010'
@@ -10,26 +10,26 @@ describe("3 node ethereum Network", function() {
 
   it("should sync the network of 3 nodes, all should be syncing the same blocks", async function() {
     const latestBlock = JSON.parse(
-      await ethereumRpcTester(
+      await ethereumRpc(
         node1RpcUrl,
         '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}'
       )
     ).result
     console.log('latestBlock', latestBlock)
     const latestBlockHashNode1 = JSON.parse(
-      await ethereumRpcTester(
+      await ethereumRpc(
         node1RpcUrl,
         '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["' + latestBlock + '", true],"id":1}'
       )
     ).hash
     const latestBlockHashNode2 = JSON.parse(
-      await ethereumRpcTester(
+      await ethereumRpc(
         node2RpcUrl,
         '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["' + latestBlock + '", true],"id":1}'
       )
     ).hash
     const latestBlockHashNode3 = JSON.parse(
-      await ethereumRpcTester(
+      await ethereumRpc(
         node3RpcUrl,
         '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["' + latestBlock + '", true],"id":1}'
       )
