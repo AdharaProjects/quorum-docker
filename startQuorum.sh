821f52f3cab2bc44407fd7e1f7cdc1d2e044a4e0
Jason Smythe <jason@smythemail.za.net>
2018-06-26 13:33:54 +0200

    Update start script to do correct checks

--- startQuorum.sh
+++ startQuorum.sh
@@ -2,7 +2,5 @@
 
 echo "in the start qnm script"
 cd /app/QuorumNetworkManager
-pwd
-ls
-KEEP_FILES=false NODE_NAME=node1 CONSENSUS=istanbul node setupFromConfig.js &
-bash /app/checkPort.sh 
+if [ -n "$DEPENDENT_IP" ]; then PORT=20010 HOST_IP=$DEPENDENT_IP ../waitForOtherNode.sh; fi
+KEEP_FILES=false NODE_NAME=node1 CONSENSUS=istanbul node setupFromConfig.js
