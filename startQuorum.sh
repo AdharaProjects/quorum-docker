2ec7eeb9a82e490e228c88274db1786b1e2fff75
Jason Smythe <jason@smythemail.za.net>
2018-06-26 16:15:54 +0200

    [experiment] see if circleci works better when docker continually prints something

--- startQuorum.sh
+++ startQuorum.sh
@@ -4,3 +4,4 @@ echo "in the start qnm script"
 cd /app/QuorumNetworkManager
 if [ -n "$DEPENDENT_IP" ]; then PORT=20010 HOST_IP=$DEPENDENT_IP ../waitForOtherNode.sh; fi
 KEEP_FILES=false NODE_NAME=node1 CONSENSUS=istanbul node setupFromConfig.js
+bash /app/checkPort.sh
